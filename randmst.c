5d6a5b724f7583d651c5ff18c78cedc380e83ea3
Julia <jpearl@college.harvard.edu>
2020-02-17 13:57:15 -0500

    graph descriptions added

--- randmst.c
+++ randmst.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Hello, World!%c", '\n');
+   return 0;
+}
+
+//three types of graphs to implement
+int graph_type_1(){
+   //2D graph, n vertices, each edge has a weight Random [0, 1]
+   return 0;
+}
+
+int graph_type_2(){
+   //2D graph, n vertices, each vertice x,y coordinate is random [0, 1], edges are distance between vertices
+   return 0;
+}
+
+int graph_type_3(){
+   //3D/4D graph, n vertices, each vertice x,y,z,?a coordinate Random [0, 1], edges are distance between vertices
    return 0;
 }
