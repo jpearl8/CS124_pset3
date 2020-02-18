e7258d33939258bfc023625de37cb0639cb94f3f
Julia <jpearl@college.harvard.edu>
2020-02-17 23:19:25 -0500

    fixed bugs

--- randmst.c
+++ randmst.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
- void populateGraph(int type, int v_count, int dimensions, int* v_array, int** adj);
+typedef struct point {
+   double x;
+   double y;
+   /* declare as many members as desired, but the entire structure size must be known to the compiler. */
+} point;
+
+int populateGraph(int type, int v_count, int dimensions, int* v_array,  point* v_array_points, double** adj);
+int graph_type_1(int v_count, int* v_array, double** adj);
+int graph_type_2(int v_count,  point* v_array, double** adj);
+
 
 
 int main(int argc, char** argv) {
@@ -11,9 +21,19 @@ int main(int argc, char** argv) {
    } else{
       int v_count = atoi(argv[2]);
       //we should probably make a graph object to initialize instead
-      int v_array[v_count]; 
-      double adj[v_count][v_count];      
-      populateGraph(atoi(argv[1]), v_count, atoi(argv[4]), v_array, adj);
+      int* v_array;
+      struct point* v_array_points;
+      if (atoi(argv[1]) == 1){
+         v_array = (int *)malloc(v_count * sizeof(int)); 
+      } else if (atoi(argv[1]) == 2){
+         v_array_points = (struct point*)malloc(v_count * sizeof(point));
+      }
+      double* adj[v_count];
+      for (int i = 0; i < v_count; i++){
+         adj[i] = (double *)malloc(v_count * sizeof(double)); 
+      }
+      // double adj[v_count][v_count];      
+      populateGraph(atoi(argv[1]), v_count, atoi(argv[4]), v_array, v_array_points, adj);
 
       //printing args
       printf("Hello, World!%c", '\n');
@@ -25,14 +45,16 @@ int main(int argc, char** argv) {
 }
 
 //calling one of the three graph types to populate vertices and edges
-void populateGraph(int type, int vertices, int dimensions, int vertices[], double adj[][]){
+int populateGraph(int type, int v_count, int dimensions, int* v_array, point* v_array_points, double** adj){
    if (type == 1){
+      printf("YES%c", '\n');
       graph_type_1(v_count, v_array, adj);
    } else if (type == 2){
-      graph_type_2(vertices);
-   } else {
-      graph_type_3(vertices, dimensions);
+      graph_type_2(v_count, v_array_points, adj);
+   // } else {
+   //    graph_type_3(vertices, dimensions);
    }
+   return 0;
 }
 
 
@@ -40,15 +62,20 @@ void populateGraph(int type, int vertices, int dimensions, int vertices[], doubl
 //three types of graphs to implement:
 
 //2D graph, n vertices, each edge has a weight Random [0, 1]
-int graph_type_1(int v_count, int v_array[], double adj[][]){
+int graph_type_1(int v_count, int* v_array, double** adj){
    for (int i = 0; i < v_count; i++){
          v_array[i] = i;
       }
    for (int i = 0; i < v_count; i++){
       for (int j = i; j < v_count; j++){
-         adj[j][i] = (double)rand();
-         adj[i][j] = adj[j][i];
-         printf("%d%c", adj[j][i], '\n');
+         if (i == j){
+            adj[j][j] = 0;
+         } else {
+            double special = (double)rand() / (double)((unsigned)RAND_MAX + 1);
+            adj[j][i] = special;
+            adj[i][j] = adj[j][i];
+            printf("%f%c", adj[j][i], '\n');
+         }
       }
    }
 
@@ -56,24 +83,30 @@ int graph_type_1(int v_count, int v_array[], double adj[][]){
 }
 
 
-//2D graph, n vertices of (x, y) coordinates with random [0, 1], edges are distance between vertices
-int graph_type_2(int v_count, Tuple<int,int>v_array[], int adj[][]){
+// //2D graph, n vertices of (x, y) coordinates with random [0, 1], edges are distance between vertices
+int graph_type_2(int v_count, point* v_array, double** adj){
    for (int i = 0; i < v_count; i++){
-      v_array[i] = ((double)rand(), (double)rand());
+      v_array[i] = (point) { .x = (double)rand() / (double)((unsigned)RAND_MAX + 1), .y = (double)rand() / (double)((unsigned)RAND_MAX + 1) };
+      printf("x = %f, y = %f%c", v_array[i].x, v_array[i].y, '\n');
    }
    for (int i = 0; i < v_count; i++){
       for (int j = i; j < v_count; j++){
-         (x_1, y_1) = v_array[i];
-         (x_2, y_2) = v_array[j];
-         double weight = sqrt(pow((x_1 - x_2)) + pow((y_1 - y_2)));
-         adj[j][i] = weight;
-         adj[i][j] = weight;
+         if (i == j){
+            adj[j][j] = 0;
+         } else {
+            double x_1 = v_array[i].x, y_1 = v_array[i].y;
+            double x_2 = v_array[j].x, y_2 = v_array[j].y;
+            double weight = sqrt((pow((x_1 - x_2), 2)) + (pow((y_1 - y_2), 2)));
+            adj[j][i] = weight;
+            adj[i][j] = weight;
+            printf("%f%c", adj[j][i], '\n');
+         }
       }
    }
    return 0;
 }
 
-int graph_type_3(int n, int dimensions){
-   //3D/4D graph, n vertices, each vertice x,y,z,?a coordinate Random [0, 1], edges are distance between vertices
-   return 0;
-}
+// int graph_type_3(int n, int dimensions){
+//    //3D/4D graph, n vertices, each vertice x,y,z,?a coordinate Random [0, 1], edges are distance between vertices
+//    return 0;
+// }
